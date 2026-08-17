d39183d2255b58e2ff3a6ffb847053a0c73e58e5
Karabo Charles Matsepane <kamatjhb025@student.wethinkcode.co.za>
2026-08-17 14:52:55 +0200

    FEAT: implemented TaskDB interface

--- src/main/java/data/TaskDB.java
+++ src/main/java/data/TaskDB.java
@@ -1,4 +1,18 @@
 package data;
 
+import domain.Task;
+
+import java.util.List;
+
 public interface TaskDB {
+
+    List<Task> getAllTasks();
+
+    Task getTask(int id);
+
+    void completeTask(int id);
+
+    void createTask(Task task);
+
+    void deleteTask(int id);
 }
