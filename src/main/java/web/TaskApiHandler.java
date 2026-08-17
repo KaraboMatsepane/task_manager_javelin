eca797d6b2de961dd35ea2debd6478131acdf0a8
Karabo Charles Matsepane <kamatjhb025@student.wethinkcode.co.za>
2026-08-17 16:40:42 +0200

    commented out web layer

--- src/main/java/web/TaskApiHandler.java
+++ src/main/java/web/TaskApiHandler.java
@@ -1,4 +1,19 @@
-package web;
-
-public class TaskApiHandler {
-}
+//package web;
+//
+//import data.InMemoryTaskDB;
+//import data.TaskDB;
+//import domain.Task;
+//import io.javalin.http.Context;
+//
+//public class TaskApiHandler {
+//
+//    private static final TaskDB taskDatabase = new InMemoryTaskDB();
+//
+//    public static void getAll(Context ctx) {
+//        ctx.json(taskDatabase.getAllTasks());
+//    }
+//
+//    public static void getTask(Context ctx) {
+//        int id = ctx.
+//    }
+//}
--- src/main/java/web/TaskServer.java
+++ src/main/java/web/TaskServer.java
@@ -1,4 +1,23 @@
-package web;
-
-public class TaskServer {
-}
+//package web;
+//
+//import domain.Task;
+//import io.javalin.Javalin;
+//
+//public class TaskServer {
+//
+//    private final Javalin app;
+//
+//    public TaskServer() {
+//        app = Javalin.create(config -> {
+//            config.http.defaultContentType = "application/json";
+//        });
+//
+//        app.get("/quotes", context -> QuoteApiHandler.getAll(context));
+//
+//        setUpEndpoints(app);
+//    }
+//
+//    private void setUpEndpoints(Javalin server) {
+//        server.jetty
+//    }
+//}
